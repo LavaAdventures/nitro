5b36eed93bc191ae59f425a5dd35c4145e880e7e
Lava <69821200+RealLava@users.noreply.github.com>
2023-01-07 16:24:46 -0500

    update0

--- main.js
+++ main.js
@@ -17,36 +17,37 @@ function download(filename, text) {
   element.click();
 
   document.body.removeChild(element);
-},
+} 
 var textarea = document.getElementById('a')
 if (navigator.userAgent.indexOf('Mobile') !== -1) { document.getElementById('save').style.display = "none"; textarea.style.height = "100px";
 }//resize for mobile
-function generate() {[
-var triesPerSecond = document.getElementById('speed').value //self explanatory
+function generate() { 
+const triesPerSecond = document.getElementById('speed').value //self explanatory
 getGiftCode = function () {
     let code = '';
     let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     for(var i = 0; i < 19; i++){
         code += dict.charAt(Math.floor(Math.random() * dict.length));
     }  
-with open('Nitro Codes.txt') as file) {
     for (line in file.readlines()) {
-       nitro = code;
+        nitro = line.strip('\n');
     }
-        url = f'https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true';
+        url = 'https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true';
 }
         r = requests.get(url);
 
         if (r.status_code == 200) {
-            console.log(f'\n\n Valid | {nitro}\n\n') #If the nitro code will be valid, it will console.log nitro code leaving two lines for (focus xD.;
+            console.log('[GEN] http://discord.gift/' + nitro + "\n");
+            document.getElementById('b').value += nitro;
         } else {
-            console.log(f'*', end = '')   #It will console.log '*' if (the nitro code won't be valid.
+            return; 
+        }
  
 
-    console.log('[GEN] http://discord.gift/' + code + "\n");
-                code += '\n'
-    document.getElementById('b').value += code;
-        }
+    //console.log('[GEN] http://discord.gift/' + code + "\n");
+                //code += '\n'
+    //document.getElementById('b').value += code;
+        
 getGiftCode();
 } //generates codes
 
@@ -63,6 +64,6 @@ function scroll() {
 }; // auto scroll
 var interval = setInterval(scroll, 100) //keep on making "console" scroll
 document.getElementById('clear').addEventListener("click", stop);
-}
+ 
 
 document.getElementById('generate').addEventListener("click", generate);
